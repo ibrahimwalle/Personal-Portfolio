4759bd0f0921eb8cf8a94bf9d0d68a1d144b6793
Wali <32612101+ibrahimwalle@users.noreply.github.com>
2025-07-09 22:17:21 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>My Portfolio</h1>
+      <h2>personal intro <a href="Linkt to wali phot">Wali photo</a></h2>
+      <h3>Work intro</h3>
+      <h4>contact</h4>
+    </>
+  )
+}
+
+export default App
