e21cc30eb34b3b3babcf1fade240de4332d9edc4
Wali <32612101+ibrahimwalle@users.noreply.github.com>
2025-07-12 15:59:21 +0300

    styling

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,10 +5,10 @@ const About = () => {
         <div className="md:flex items-center justify-center mx-auto my-7 p-6 gap-3 h-full min-h-fit md:w-5xl"> 
             <div className="flex flex-col items-center justify-center w-full">
                 <img src="/ava.jpg" alt="Profile" className="rounded-full mb-6 w-2xs" />
-                <span className="font-bold text-center text-sm md:block lg:block hidden">Software Engineer <br /> AI Specialist</span>
+                <span className="font-bold text-center text-2xl mb-4">Software Engineer <br /> AI Specialist</span>
             </div>
             <div className="text-center w-full">
-                <h1 className="text-4xl font-bold mb-4">About Me</h1>
+                <h1 className="text-2xl font-bold mb-4 hidden md:block lg:block">About Me</h1>
                 <p className="text-lg w-full">
                     Hi, I’m Ibrahim Al Wali a software engineer who’s just as comfortable in the outdoors as I am behind a screen. When I’m not coding, I’m out capturing landscapes through my lens, setting up camp under the stars, or enjoying a quiet moment fishing. 
                 </p>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
                         <a href="https://www.linkedin.com/in/ibrahim-alwali/" target='blank' className="text-4xl font-bold">IBRAHIM WALI</a>
                     </div>
                     <div className="ml-auto md:hidden flex items-center justify-start">
-                        <button className="tap-highlight-transparent text-black dark:text-white w-5 h-5 relative focus:outline-none bg-transparent" onClick={menuToggle}>
+                        <button className="tap-highlight-transparent text-black w-5 h-5 relative focus:outline-none bg-transparent" onClick={menuToggle}>
                             <span className="sr-only">Open main menu</span>
                             <div className="block w-5 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
                                 <span aria-hidden="true" className={`block absolute h-0.5 w-5 bg-current transform transition duration-500 ease-in-out ${open ? 'rotate-45' : '-translate-y-1.5'}`}></span>
